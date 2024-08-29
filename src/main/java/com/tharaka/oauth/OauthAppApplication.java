9eaf76127f31501fff618762b082bd4ace927c80
Tharaka Jayasinghe <124444782+tharaka-jayasinghe-7@users.noreply.github.com>
2024-08-29 23:43:15 +0530

    Main
    first commit

---
+++ src/main/java/com/tharaka/oauth/OauthAppApplication.java
@@ -0,0 +1,13 @@
+package com.tharaka.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tharaka/oauth/OauthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharaka.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
